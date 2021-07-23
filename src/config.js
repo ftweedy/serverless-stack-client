0ba0c45b34602a92b53964c3b094c02498ee476b
Francis Tweedy <tweedyfrancis@gmail.com>
2021-07-23 16:47:54 -0700

    adding aws Amplify to connect front-end to amazon services

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "tweedy-notes",
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://udoyqpowad.execute-api.us-east-1.amazonaws.com/prod",
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_n3iSVYRBy",
+      APP_CLIENT_ID: "Y15mqsh0qq0ldp4q6clfndiqmmm",
+      IDENTITY_POOL_ID: "us-east-1:36b4bd30-077b-4f43-b9ac-f7a209c0bd0b",
+    },
+  };
+  
+  export default config;
