0407f896f13e183776c6a4dc3c35521c3f0e973e
Francis Tweedy <tweedyfrancis@gmail.com>
2021-07-27 14:39:40 -0700

    added useHistory for basic routing for logging in and out

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import { Auth } from "aws-amplify";
@@ -11,6 +12,7 @@ import { AppContext } from "./libs/contextLib";
 function App() {
   const [isAuthenticated, userHasAuthenticated] = useState(false);
   const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const history = useHistory();
 
   useEffect(() => {
     onLoad();
@@ -33,6 +35,7 @@ function App() {
   async function handleLogout() {
     await Auth.signOut();
     userHasAuthenticated(false);
+    history.push("/login");
   }
   
   return (
